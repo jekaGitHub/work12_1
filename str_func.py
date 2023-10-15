0988eaff6defc204ecd859b410a2c64a40fa84d2
Evgeny Safonov <jeka-luda@yandex.ru>
2023-10-16 00:51:25 +0300

    add function and add docstring

--- str_func.py
+++ str_func.py
@@ -1,3 +1,9 @@
 def str_upper(str):
+"""преобразует в верхний регистр"""
 	return str.upper()
+
+
+def str_capitalize(str):
+"""Преобразует первые символы слов в верхний регистр"""
+	return str.title()
 
